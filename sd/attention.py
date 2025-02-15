fa7782f30c0f6cd1623279d8f1f5b46765e8de51
Ganesh Nagaraja <ganeshnagaraja@HMECL-GD4QLQ6L4.local>
2025-02-15 16:41:45 +0530

    clip and diffusion scripts added/updated

--- sd/attention.py
+++ sd/attention.py
@@ -57,6 +57,62 @@ class SelfAttention(nn.Module):
 
 
         
+class CrossAttention(nn.Module):
+
+    def __init__(self, n_heads: int, d_embed: int, d_cross: int, in_proj_bias = True, out_proj_bias = True):
+        super().__init__()
+
+        self.q_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor, causal_mask=False):
+        # x: latent: (batch_size, seq_len, Dim)
+        # y: (context): (batch_size, seq_len_kv, dim_kv) = (batch_size, 77, 768)
+
+        
+        input_shape = x.shape
+        batch_size, seq_len, d_embed = input_shape
+
+        interim_shape = (batch_size, -1, self.n_heads, self.d_head)
+
+        # (Batch_size, seq_len, dim) --> (Batch_size, seq_len, dim * 3) --> 3 tensors of shape (Batch_size, seq_len, dim)
+        q, k , v = self.in_proj(x).chunk(3, dim=-1)
+
+        # (Batch_size, seq_len, Dim) --> (batch_size, seq_len, H, dim / H) --> (batch_size, H, seq_len, dim / H)
+        q = q.view(interim_shape).transpose(1, 2)
+        k = k.view(interim_shape).transpose(1, 2)
+        v = v.view(interim_shape).transpose(1, 2)
+
+        # (batch_size, H, seq_len, seq_len)
+        weight = q @ k.transpose(-1, -2)
+
+        if causal_mask:
+            # Mask where the upper triangle (values above the diagonal) is made up of 1
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
+            weight.masked_fill(mask, -torch.inf)
+
+        weight /= math.sqrt(self.d_head)
+
+        weight = F.softmax(weight, dim=-1)
+
+        # (batch_size, H, seq_len, seq_len) @ (batch_size, H, seq_len, dim / H) -> (batch_size, H, seq_len, dim / H)
+        output = weight @ v
+        
+        # (batch_size, H, seq_len, dim / H)  --> (batch_size, seq_len, H, dim / H)
+        output = output.transpose(1, 2)
+
+        # (batch_size, seq_len, H, dim / H) --> (batch_size, seq_len, dim)
+        output = output.reshape(input_shape)
+
+        output = self.out_proj(output)
+
+        # (batch_size, seq_len, dim)
+        return output
 
 
 
--- sd/clip.py
+++ sd/clip.py
@@ -2,3 +2,82 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from attention import SelfAttention
+
+class CLIPEmbedding(nn.Module):
+    def __init__(self, n_vocab: int, n_embed: int, n_tokens: int):
+
+        self.token_embedding = nn.Embedding(n_vocab, n_embed)
+        self.position_embedding = nn.Embedding(torch.zeros(n_tokens, n_embed))
+
+    def forward(self, tokens):
+        # (Batch_size, seq_len) -> (Batch_size, seq_len, dim)
+        x = self.token_embedding(tokens)
+
+        x += self.position_embedding
+
+        return x
+    
+class CLIPLayer(nn.Module):
+
+    def __init__(self, n_head: int, n_embed: int):
+
+        self.layernorm_1 = nn.LayerNorm(n_embed)
+        self.attention = SelfAttention(n_head, n_embed)
+        self.layernorm_2 = nn.LayerNorm(n_embed)
+        self.linear_1 = nn.Linear(n_embed, 4 * n_embed)
+        self.linear_2 = nn.Linear(4 * n_embed, n_embed)
+
+    def forward(self, x: torch.Tensor):
+        # (batch_size, seq_len, dim)
+        residue = x 
+
+        ## Self Attention
+
+        x = self.layernorm_1(x)
+
+        x = self.attention(x, causal_mask=True)
+
+        x += residue
+
+        ## Feedforward Layer
+
+        residue = x
+
+        x = self.layernorm_2(x)
+
+        x = self.linear_1(x)
+
+        x = x * torch.sigmoid(1.702 * x)
+
+        x = self.linear_2(x)
+
+        x += residue
+
+        return x
+
+class CLIP(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.embedding = CLIPEmbedding(49408, 768, 77) # (no_of_words (vocab_size), dim, seq_len)
+        
+        self.layers = nn.Module([
+            CLIPLayer(12, 768) for i in range(12)
+        ])
+
+        self.layernorm = nn.LayerNorm(768)
+
+    def forward(self, tokens:torch.LongTensor):
+        tokens = tokens.type(torch.long)
+
+        # (Batch_size, seq_len) -> (Batch_size, seq_len, dim)
+        state = self.embedding(tokens)
+
+        for layer in self.layers:
+            state = layer(state)
+
+        # (Batch_size, seq_len, dim)
+        output = self.layernorm(state)
+
+        return output
+    
